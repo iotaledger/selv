c485c50c37c47653d935f4ddc09a0d1fff80753a
Umair Sarfraz <aquadestructor@icloud.com>
2020-02-03 16:32:32 +0500

    feat: setup boilerplate for mobile applications

---
+++ mobile/README.md
@@ -0,0 +1,7 @@
+# Digital Identity Narratives as a Service PoC
+
+#### Run in development mode
+
+```
+yarn dev
+```
---
+++ mobile/ui/App.svelte
@@ -0,0 +1,12 @@
+<main>
+	<h1>Welcome!</h1>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+</style>
