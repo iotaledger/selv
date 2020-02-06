893f6dec1017480e9636acaf0b1f2e77f816d27d
Alexey Sobolev <sobolev@mail.de>
2020-02-06 14:57:56 +0100

    feat: Added cookie disclaimer

---
+++ web/src/components/Disclaimer.tsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react'
+import { withCookies } from 'react-cookie';
+import { Button } from 'antd';
+
+// import '../assets/styles/disclaimer.scss'
+
+const Disclaimer = ({ cookies }: { cookies: any }) => {
+  const [ack, setAck] = useState(true)
+
+  useEffect(() => {
+    const ack = cookies.get('didi-cookie');
+    if (!ack && document) {
+      const element = document.getElementById('footer');
+      if (element) {
+        element.classList.add('cookie-bar-bottom-bar');
+        setAck(false)
+      }
+    }
+  }, [])
+
+  function dismiss() {
+    cookies.set('didi-cookie', true, { path: '/' });
+    if (document) {
+      const element = document.getElementById('footer');
+      if (element) {
+        element.classList.remove('cookie-bar-bottom-bar');
+        setAck(true)
+      }
+    }
+  }
+
+  if (ack) return null;
+
+  return (
+    <div className="disclaimer-wrapper">
+      <span className="disclaimer-text">
+        This website uses cookies to ensure you get the best experience on our
+        website.&nbsp;
+        <a
+          className="disclaimer-link"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.iota.org/research/privacy-policy"
+        >
+          Learn more
+        </a>
+      </span>
+      <Button className="primary" onClick={dismiss}>Dismiss</Button>
+    </div>
+  )
+}
+
+export default withCookies(Disclaimer)
