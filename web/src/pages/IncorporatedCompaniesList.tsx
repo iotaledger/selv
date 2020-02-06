2102081e1f748acf8a4e9723ab6fb6fd4bc97ea4
Alexey Sobolev <sobolev@mail.de>
2020-02-06 14:59:35 +0100

    refactor: Refactored to use layout wrapper

--- web/src/pages/IncorporatedCompaniesList.tsx
+++ web/src/pages/IncorporatedCompaniesList.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from 'react-router-dom'
 import { Button } from 'rsuite';
 import useStep from "../utils/useStep";
-import { Steps, Sidebar, Table } from "../components";
+import { Layout, Table } from "../components";
 import companies from "../incorporatedCompanies.json"
 import logo from '../assets/companyHouse.svg'
 
@@ -10,35 +10,27 @@ import logo from '../assets/companyHouse.svg'
  * Component which will display a CompanyIntro.
  */
 const IncorporatedCompanies: React.FC = ({ history, match }: any) => {
-    const { step, nextStep, mainSteps } = useStep(match); 
+    const { nextStep } = useStep(match); 
 
     function onRowClick(data: any) {
         history.push(`/details/company/${data.id}`)
     }
 
     return (
-        <div className="page-wrapper">
-            <div className="main-section">
-                <div className="companies-page-wrapper">
-                    <img src={logo} alt="Company House Logo" />
-                    <div className="companies-cta-wrapper">
-                        <h4>Newly Incorporated Companies</h4>
-                        <Link to={nextStep}>
-                            <Button size="lg" appearance="subtle" active>
-                                Register New Company
-                            </Button> 
-                        </Link>
-                    </div>
-                    <Table data={companies} onRowClick={onRowClick} />
+        <Layout theme="companyHouse" match={match}>
+            <div className="companies-page-wrapper">
+                <img src={logo} alt="Company House Logo" />
+                <div className="companies-cta-wrapper">
+                    <h4>Newly Incorporated Companies</h4>
+                    <Link to={nextStep}>
+                        <Button size="lg" appearance="subtle" active>
+                            Register New Company
+                        </Button> 
+                    </Link>
                 </div>
+                <Table data={companies} onRowClick={onRowClick} />
             </div>
-            <Sidebar>
-                <Steps 
-                    steps={mainSteps} 
-                    stepId={step} 
-                />
-            </Sidebar>
-        </div>
+        </Layout>
     );
 }
 
