c75f3072a091fb5680f107fc5a1403887e551690
Alexey Sobolev <sobolev@mail.de>
2020-02-08 00:40:00 +0100

    feat: Footer component

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import companyHouse from '../assets/companyHouseCrone.svg'
+import snsBank from '../assets/snsBank.svg'
 
 const Footer = ({ children, theme }: {
     children?: JSX.Element | null | undefined;
@@ -8,7 +9,10 @@ const Footer = ({ children, theme }: {
     return (
         <div className="footer-wrapper">
             {children}
-            { theme === 'companyHouse' && <img src={companyHouse} alt="Company House Logo" /> }
+            <div className="logo">
+                { theme === 'companyHouse' && <img src={companyHouse} alt="Company House Logo" /> }
+                { theme === 'snsBank' && <img src={snsBank} alt="SNS Bank Logo" /> }
+            </div>
         </div>
     );
 };
