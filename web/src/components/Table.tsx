f227dbf031188efa6ee7472e1d829009bc3e6c11
Alexey Sobolev <sobolev@mail.de>
2020-02-08 00:40:27 +0100

    feat: Table component

--- web/src/components/Table.tsx
+++ web/src/components/Table.tsx
@@ -6,31 +6,81 @@ const { Column, HeaderCell, Cell } = Table;
 
 // https://rsuitejs.com/en/components/table#%3CTable%3E
 
+const CompanyCell = ({ ...props }) => {
+    return (
+        <Cell {...props} style={{ 
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center'
+        }} >
+            <div className="cell-content">
+                <p className="company-name">
+                    {props.rowData.name}
+                </p>
+                <p className="company-type">
+                    {props.rowData.type}
+                </p>
+            </div>
+        </Cell>
+    );
+};
+
+const StatusCell = ({ ...props }) => {
+    return (
+        <Cell {...props} style={{ 
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center'
+        }} >
+            <div className="cell-content status-wrapper">
+                <p className={`status ${props.rowData.status.toLowerCase()}`}>
+                    {props.rowData.status}
+                </p>
+            </div>
+        </Cell>
+    );
+};
+
 const TableInstance = ({ data, onRowClick }: {
     data?: { name: string, date: string, type: string, status: string }[];
     onRowClick: (rowData: object) => void
 }) => {
     return (
         <div className="table-wrapper">
-            <Table width={730} height={370} data={data} onRowClick={onRowClick}>
-                <Column width={200} fixed>
+            <Table width={710} height={570} data={data} rowHeight={90} onRowClick={onRowClick}>
+                <Column width={300} fixed>
                     <HeaderCell>Company Name</HeaderCell>
-                    <Cell dataKey="name" />
+                    <CompanyCell />
                 </Column>
 
                 <Column width={200} fixed>
                     <HeaderCell>Incorporated on</HeaderCell>
-                    <Cell dataKey="date" />
+                    <Cell dataKey="date" style={{ 
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center'
+                    }} />
                 </Column>
 
-                <Column width={230} fixed>
-                    <HeaderCell>Type</HeaderCell>
-                    <Cell dataKey="type" />
-                </Column>
-
-                <Column width={100} fixed>
+                <Column width={130} fixed>
                     <HeaderCell>Status</HeaderCell>
-                    <Cell dataKey="status" />
+                    <StatusCell />
+                </Column>
+
+                <Column width={50} fixed>
+                    <HeaderCell></HeaderCell>
+                    <Cell className="dots" style={{ 
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        fontSize: 30,
+                        position: 'relative',
+                        top: -8
+                    }} >
+                        <div className="cell-content">
+                            ...
+                        </div>
+                    </Cell>
                 </Column>
             </Table>
       </div>
