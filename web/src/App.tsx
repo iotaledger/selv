beb0bed215948ce695e058eccac8cc3007a6b917
Alexey Sobolev <sobolev@mail.de>
2020-02-08 00:50:20 +0100

    feat: Added webfontloader and GA

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import WebFontLoader from 'webfontloader';
+import ReactGA from 'react-ga';
 import { Landing, CompanyDetails } from './pages'
 import GlobalState from './context/globalState'
 import 'rsuite/lib/styles/index.less';
 import 'react-toastify/dist/ReactToastify.css';
-import './App.css';
+import './styles/index.scss';
 import { routes } from './steps'
 
 toast.configure();
+
+WebFontLoader.load({
+  google: {
+      families: ['Open Sans:300,400,600,700', 'Maven Pro:300,400,600,700'],
+  },
+});
+
+ReactGA.initialize('222308434'); // (trackingID, { debug: true })
+ReactGA.set({ anonymizeIp: true });
 
 const App: React.FC = () => {
   return (
