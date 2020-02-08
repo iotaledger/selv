88d42e7e457357470f7cde58e7706a0f0d6afdd3
Alexey Sobolev <sobolev@mail.de>
2020-02-08 20:07:11 +0100

    feat: Scan QR code page

---
+++ web/src/pages/AppDownloadQR.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'antd';
+import { Layout, QRCode } from "../components";
+import useStep from "../utils/useStep";
+import appStore from '../assets/appStore.svg'
+import googlePlay from '../assets/googlePlay.svg'
+
+/**
+ * Component which will display a AppDownloadQR.
+ */
+const AppDownloadQR: React.FC = ({ match }: any) => {
+    const { nextStep } = useStep(match); 
+
+    const link = 'https://trinity.iota.org/'
+    const [qrContent] = useState(link);
+
+    return (
+        <Layout theme="companyHouse" match={match} step={2}>
+            <div className="scan-qr-page-wrapper">
+                <h1>Download the Selv app</h1>
+                <p>Scan this QR code with you camera app to download the app</p>
+                <QRCode text={qrContent} />
+
+                <div className="app-cta-wrapper">
+                    <a 
+                        href='https://apps.apple.com/us/app/trinity-wallet/id1385929472?ls=1'
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img src={appStore} alt="Apple AppStore" />
+                    </a>
+                    <a 
+                        href='https://play.google.com/store/apps/details?id=com.iota.trinity'
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img src={googlePlay} alt="Google Play Market" />
+                    </a>
+                </div>
+
+                <p>Press <strong>Continue</strong> when you have the Selv app on your phone</p>
+                <Link to={nextStep} className="cta">
+                    <Button>
+                        Continue
+                    </Button> 
+                </Link>
+            </div>
+        </Layout>
+    );
+}
+
+export default AppDownloadQR;
