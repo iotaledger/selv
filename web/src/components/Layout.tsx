68f0cf331e351d78b48c7f4b1eb337dc9b048d1d
Alexey Sobolev <sobolev@mail.de>
2020-02-08 21:46:45 +0100

    refactor: removed unused imports

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import ReactGA from 'react-ga';
 import Disclaimer from './Disclaimer'
 import Header from './Header'
