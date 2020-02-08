3218529e5fe0764cb2debac8f32d0f2d7d493e2f
Alexey Sobolev <sobolev@mail.de>
2020-02-08 20:05:25 +0100

    feat: Company confirmation page

--- web/src/pages/CompanyConfirmation.tsx
+++ web/src/pages/CompanyConfirmation.tsx
@@ -1,33 +1,33 @@
 import React from "react";
 import { Link } from 'react-router-dom'
-import { Button } from 'rsuite';
+import { Button } from 'antd';
 import useStep from "../utils/useStep";
-import { Steps, Sidebar } from "../components";
+import { Layout } from "../components";
+import checkmark from '../assets/checkmark.svg'
+import selv from '../assets/selv.svg'
 
 /**
  * Component which will display a CompanyConfirmation.
  */
 const CompanyConfirmation: React.FC = ({ match }: any) => {
-    const { step, subStep, nextStep, subSteps, mainSteps } = useStep(match); 
+    const { nextStep } = useStep(match); 
 
     return (
-        <div className="page-wrapper">
-            <div className="main-section">
-                <h1>CompanyConfirmation</h1>
+        <Layout theme="companyHouse" match={match} step={2}>
+            <div className="company-confirmation-page">
+                <img src={checkmark} alt="Success checkmark" />
+                <h2>Congratulations,<br/>your company is now set up!</h2>
+                <div className="selv-wrapper">
+                    <img src={selv} alt="Selv app logo" />
+                    <h3>Your new credentials are sent to Selv app</h3>
+                </div>
                 <Link to={nextStep}>
-                    <Button size="lg">
-                        Next Page
+                    <Button>
+                        Continue
                     </Button> 
                 </Link>
             </div>
-            <Sidebar>
-                <Steps 
-                    steps={mainSteps} 
-                    stepId={step} 
-                    subSteps={<Steps steps={subSteps} stepId={subStep} />}
-                />
-            </Sidebar>
-        </div>
+        </Layout>
     );
 }
 
