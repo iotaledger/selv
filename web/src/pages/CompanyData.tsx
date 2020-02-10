286804cfbdb9dc95654187faf16739b9349a38bc
Alexey Sobolev <sobolev@mail.de>
2020-02-10 18:32:35 +0100

    feat: company data page

--- web/src/pages/CompanyData.tsx
+++ web/src/pages/CompanyData.tsx
@@ -1,9 +1,10 @@
 import React, { useState , useEffect } from "react";
-import { Link } from 'react-router-dom'
-import { Button } from 'antd';
+import SocketIOClient from 'socket.io-client';
+import axios from 'axios';
+import { serverAPI, websocketURL } from '../config.json'
 import useStep from "../utils/useStep";
 import { flattenObject } from "../utils/helper";
-import { Layout, PrefilledUserDataForm } from "../components";
+import { Layout, Form, PrefilledForm } from "../components";
 
 const prefilledFields = [
     'FirstName',
@@ -15,6 +16,15 @@ const prefilledFields = [
     'Phone',
 ]
 
+const emptyFields = [
+    'CompanyName',
+    'CompanyAddress',
+    'CompanyType',
+    'CompanyBusiness',
+]
+
+const shortFields = ['Date', 'Nationality']
+
 const labels = {
     FirstName: 'First Name',
     LastName: 'Last Name',
@@ -25,19 +35,30 @@ const labels = {
     Country: 'Country of residence',
     Phone: 'Phone number',
     Address: 'Address',
+    CompanyName: 'Company name',
+    CompanyAddress: 'Company address',
+    CompanyType: 'Company type',
+    CompanyBusiness: 'Nature of business'
 }
 
 /**
  * Component which will display a CompanyData.
  */
-const CompanyData: React.FC = ({ match }: any) => {
+const CompanyData: React.FC = ({ history, match }: any) => {
     const { nextStep } = useStep(match); 
     const [prefilledData, setPrefilledData] = useState({})
+
+    let ioClient: any
 
     useEffect(() => {
         async function getData() {
             const credentialsString: string | null = await localStorage.getItem('credentials')
             const credentials = credentialsString && await JSON.parse(credentialsString)
+            const status = credentials?.status
+            if (!status || Number(status) !== 2) {
+                console.log('Credentials missing or not trusted')
+                history.goBack()
+            }
             const flattenData = flattenObject(credentials?.data)
             const address = { Address: `${flattenData.Street} ${flattenData.House}, ${flattenData.City}, ${flattenData.Country}, ${flattenData.Postcode}` }
             const result = prefilledFields.reduce((acc: any, entry: string) => 
@@ -46,22 +67,86 @@ const CompanyData: React.FC = ({ match }: any) => {
             setPrefilledData({ ...result, ...address })
         } 
         getData()
+
+        // Removing the listener before unmounting the component in order to avoid addition of multiple listener
+        return () => {
+            ioClient && console.log('WebSocket disconnected');
+            ioClient?.off('createCredentialConfirmation');
+            ioClient?.off('error');
+            ioClient?.disconnect();
+        }
     }, [])
 
-    const data: any = { dataFields: prefilledData, labels }
+
+    async function connectWebSocket(channelId: string, data: object) {
+        ioClient = SocketIOClient(websocketURL, {
+            reconnection: true,
+            reconnectionDelay: 500,
+            jsonp: false,
+            reconnectionAttempts: Infinity,
+            transports: ['websocket']
+        })
+
+        ioClient.emit('registerDesktopClient', { channelId })
+
+        console.log('emit createCredential')
+        const payload = {
+            schemaName: 'Company', 
+            data
+        }
+        ioClient.emit('createCredential', { channelId, payload })
+    
+        ioClient.on('error', async (error: any) => {
+            console.error('WebSocket error', error)
+        })
+
+        ioClient.on('createCredentialConfirmation', async (payload: any) => {
+            console.log('createCredentialConfirmation', payload)
+        })
+    } 
+
+    async function getChannelId() {
+        const storedChannelDetails = await localStorage.getItem('WebSocket_DID') || null;
+        const channelDetails = storedChannelDetails && JSON.parse(storedChannelDetails);
+        return channelDetails?.channelId
+    }
+
+    async function checkConnectedStatus(channelId: string) {
+        const response = await axios.get(`${serverAPI}/connection?channelId=${channelId}`)
+        return response && response?.data?.status === 'success'
+    }
+
+    async function processValues(fields: object) {
+        console.log('page got data', fields)
+        const channelId = await getChannelId()
+        console.log('got channelId', channelId)
+        if (channelId) {
+            const isMobileConnected = await checkConnectedStatus(channelId)
+            console.log('isMobileConnected', isMobileConnected)
+            if (isMobileConnected) {
+                await connectWebSocket(channelId, fields);
+            }
+        } else {
+            console.log('No websocket connection details')
+        }
+    }
+
+    const prefilledFormData: any = { dataFields: prefilledData, labels, shortFields }
+    const emptyFormData: any = { dataFields: emptyFields, labels, processValues }
 
     return (
         <Layout theme="companyHouse" match={match} step={2}>
-            <div className="scan-qr-page-wrapper">
-                <h1>CompanyData</h1>
+            <div className="company-data-page-wrapper">
+                <h2>Set up a private limited company</h2>
+                <h3 className="section-header">Business owner</h3>
                 {
-                    Object.keys(data.dataFields).length && <PrefilledUserDataForm { ...data } />
+                    Object.keys(prefilledFormData.dataFields).length && 
+                    <PrefilledForm { ...prefilledFormData } />
                 }
-                <Link to={nextStep}>
-                    <Button>
-                        Next Page
-                    </Button> 
-                </Link>
+                <p className="notice bold small">Credentials provided by Selv ID</p>
+
+                <h3 className="section-header">Company Details</h3>
+                <Form { ...emptyFormData } />
             </div>
         </Layout>
     );
