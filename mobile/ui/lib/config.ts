0990105ac0dd4829b40eee1b535933b31cc864b9
Umair Sarfraz <aquadestructor@icloud.com>
2020-02-10 16:08:17 +0500

    chore: update API_URL port

--- mobile/ui/lib/config.ts
+++ mobile/ui/lib/config.ts
@@ -1,6 +1,6 @@
 export const WEBSOCKETS_URL: string = 'http://localhost:8000';
 
-export const API_URL: string = 'http://localhost:8000';
+export const API_URL: string = 'http://localhost:8080';
 
 export const IOTA_NODE_URL: string = 'https://nodes.devnet.iota.org:443';
 
