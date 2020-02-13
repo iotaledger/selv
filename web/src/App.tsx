87bec2be8863530a6b341cb18b6730af4c773173
Alexey Sobolev <sobolev@mail.de>
2020-02-13 01:15:10 +0100

    feat: new routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import WebFontLoader from 'webfontloader';
 import ReactGA from 'react-ga';
-import { Landing, CompanyDetails } from './pages'
+import { Landing, IncorporatedCompanies, CompanyDetails } from './pages'
 import GlobalState from './context/globalState'
 import 'antd/dist/antd.css';
 import 'rsuite/lib/styles/index.less';
@@ -29,7 +29,8 @@ const App: React.FC = () => {
               <Route exact key={path} path={path} component={page} />
             )
           }
-          <Route exact path={'/details/company/:companyId'} component={CompanyDetails} />
+          <Route path={'/progress/company/details/:step/:companyId'} component={CompanyDetails} />
+          <Route path={'/progress/company/list/:step'} component={IncorporatedCompanies} />
           <Route component={Landing} />
         </Switch>
       </BrowserRouter>
