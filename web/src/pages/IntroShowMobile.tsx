27ea48288e392f7dddc0ca04eac951c4d03198fe
Alexey Sobolev <sobolev@mail.de>
2020-02-14 19:17:59 +0100

    feat: intro mobile page

--- web/src/pages/IntroShowMobile.tsx
+++ web/src/pages/IntroShowMobile.tsx
@@ -13,15 +13,22 @@ const IntroShowTodos: React.FC = ({ match }: any) => {
     
     return (
         <Layout match={match}>
-            <React.Fragment>
-                <h2>Looks like a lot of work,<br />unless Bob is using Selv 🧐</h2>
-                <img src={mobile} alt="Mobile phone" />
-                <Link to={nextStep}>
-                    <Button type="primary">
-                        Let's do it 
-                    </Button> 
-                </Link>
-            </React.Fragment>
+            <div className="demo-intro">
+                <div className="app">
+                    <h1>That looks like a lot of work...</h1>
+                    <h3>It’s a pretty long to-do list for today, but using IOTA’s Unified Identity Protocol and the Selv app,  it will be ​quick and easy​.</h3>
+                    <div className="info">
+                        <p>This demo can be best experienced using the browser on a PC in combination with the Selv app on your phone. A full phone experience is possible using the mobile browser and the Selv app.</p>
+                        
+                        <Link to={nextStep}>
+                            <Button type="primary">
+                                Let's get started 
+                            </Button> 
+                        </Link>
+                    </div>
+                    <img className="phone" src={mobile} alt="Mobile phone" />
+                </div>
+            </div>
         </Layout>
     );
 }
