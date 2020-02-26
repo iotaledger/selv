d9b078a22e5b94fa087d4bece8782c62d1a3a36c
Alexey Sobolev <sobolev@mail.de>
2020-02-26 17:57:59 +0100

    feat: footer content

--- web/src/components/landing/Footer.tsx
+++ web/src/components/landing/Footer.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import logo from '../../assets/landing/logoFooter.svg' 
 
 export default ({ }) => (
     <div className="footer-section">
+        <img src={logo} alt="Selv logo" />
         <h1>Contact us</h1>
+        <p>Would you like to know how Self Sovereign Identity can positively influence<br />your business or would you like to experiment with the technology?</p>
+        <p className="bold">Contact us at <a href="mailto:UIP@iota.org" className="bold">UIP@iota.org</a></p>
+        <div className="footer-links">
+            <a
+                className="footer-link"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.iota.org"
+            >
+                Visit iota.org
+            </a>
+            <a
+                className="footer-link"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.iota.org/research/privacy-policy"
+            >
+                Privacy Policy
+            </a>
+        </div>
     </div>
 );
