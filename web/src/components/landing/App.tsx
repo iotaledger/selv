2e1911e0b95ec704ff748a0a1ba91a91d8a5636e
Alexey Sobolev <sobolev@mail.de>
2020-02-27 15:34:08 +0100

    feat: added selv logo

--- web/src/components/landing/App.tsx
+++ web/src/components/landing/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import { Button } from 'antd';
 import { RandomGraphicElement } from '../'
+import selv from '../../assets/selv.svg' 
 import app from '../../assets/landing/app.png' 
 import appMobile from '../../assets/landing/app-mobile.png' 
 
@@ -16,15 +17,24 @@ export default ({ }) => (
                 <img src={appMobile} alt="App" className="mobile" />
                 <div className="content-list">
                     <div className="content-item">
-                        <h5>Self Sovereign Identity</h5>
+                        <span className="content-header-wrapper">
+                            <img src={selv} alt="" />
+                            <h5>Self Sovereign Identity</h5>
+                        </span>
                         <p>Through Self Sovereign Identity, people, organisations and things are empowered to collect and share their own verified data and digital identity.</p>
                     </div>
                     <div className="content-item">
-                        <h5>One App – One Protocol</h5>
+                        <span className="content-header-wrapper">
+                            <img src={selv} alt="" />
+                            <h5>One App – One Protocol</h5>
+                        </span>
                         <p>This solution provides an easy and cheap way to comply with GDPR and similar privacy laws.</p>
                     </div>
                     <div className="content-item">
-                        <h5>A Unified Identity</h5>
+                        <span className="content-header-wrapper">
+                            <img src={selv} alt="" />
+                            <h5>A Unified Identity</h5>
+                        </span>
                         <p>The Unified Identity Protocol not only enables people and organisations to have the benefits of identity, but devices, opening up a whole new world of business models and use cases.</p>
                     </div>
                 </div>
