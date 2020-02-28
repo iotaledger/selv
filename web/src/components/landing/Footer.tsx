9fbf4e890fda0f7440a2bbcb8db90172a2853156
Alexey Sobolev <sobolev@mail.de>
2020-02-28 16:43:59 +0100

    feat: footer section content update

--- web/src/components/landing/Footer.tsx
+++ web/src/components/landing/Footer.tsx
@@ -5,7 +5,7 @@ export default ({ }) => (
     <div className="footer-section">
         <img src={logo} alt="Selv logo" />
         <h1>Contact us</h1>
-        <p>Would you like to know how Self Sovereign Identity can positively influence<br />your business or would you like to experiment with the technology?</p>
+        <p>Would you like to know how Self Sovereign Identity can positively influence<br className="line-break"/>your business or would you like to experiment with the technology?</p>
         <p className="bold">Contact us at <a href="mailto:UIP@iota.org" className="bold">UIP@iota.org</a></p>
         <div className="footer-links">
             <a
