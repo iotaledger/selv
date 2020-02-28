14a526bda3b3a83c2bf97ab5dd3bfd30af4fef12
Alexey Sobolev <sobolev@mail.de>
2020-02-28 16:58:38 +0100

    feat: controll identity section: added fade-up effect

--- web/src/components/landing/ControlIdentity.tsx
+++ web/src/components/landing/ControlIdentity.tsx
@@ -6,14 +6,14 @@ export default ({ }) => (
     <RandomGraphicElement elements={5}>
         <div className="control-identity-section">
             <div className="content-wrapper">
-                <div className="text-content-wrapper">
+                <div className="text-content-wrapper" data-aos="fade-up">
                     <div className="heading-wrapper">
                         <h2>You control your</h2>
                         <h2>own identity.</h2>
                     </div>
                     <p>Through Self Sovereign Identity (SSI), people, organisations and things are empowered to collect and share their own verified data and digital identity. This increased privacy and control enables a “Bring Your Own Identity” (BYOI) style of engagement with corporations, removing the need to perform expensive data collection.</p>
                 </div>
-                <img src={connected} alt="Connected" />
+                <img data-aos="fade-up" src={connected} alt="Connected" className="illustration"/>
             </div>
         </div>
     </RandomGraphicElement>
