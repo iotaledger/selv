0fa6fb6658abc675a4354e483c1e01d1acbd916b
Alexey Sobolev <sobolev@mail.de>
2020-03-02 16:19:38 +0100

    feat: intro todos page content update

--- web/src/pages/IntroShowTodos.tsx
+++ web/src/pages/IntroShowTodos.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from "react";
 import { Link } from 'react-router-dom'
 import { Button } from 'antd';
+import { RandomGraphicElement } from '../components'
 import useStep from "../utils/useStep";
-import { Layout, Steps } from "../components";
+import howItWorks from '../assets/landing/howItWorks1.png' 
 
 /**
  * Component which will display a IntroShowTodos.
  */
 const IntroShowTodos: React.FC = ({ match }: any) => {
-    const { nextStep, mainSteps } = useStep(match); 
+    const { nextStep } = useStep(match); 
     
     useEffect(() => {
         const reset = async () => await localStorage.clear()
@@ -16,23 +17,25 @@ const IntroShowTodos: React.FC = ({ match }: any) => {
     }, [])
 
     return (
-        <Layout match={match}>
-            <div className="demo-intro">
-                <div className="todos">
-                    <h1>Welcome to the Selv demo for setting up your own company.</h1>
-                    <h3>Here is your to-do list for today:</h3>
-                    <Steps 
-                        steps={mainSteps} 
-                        stepId={2} 
-                    />
+        <RandomGraphicElement elements={7}>
+            <div className="theme-demo">
+                <div className="demo-intro">
+                    <h2>Welcome to the <span className="highlight">Selv demo</span></h2>
+                    <h3>Here is <strong>your to-do list</strong> for today:</h3>
+                    <ul className="todos">
+                        <li>Set up a company</li>
+                        <li>Get a bank account</li>
+                        <li>Get liability insurance</li>
+                    </ul>
                     <Link to={nextStep}>
-                        <Button type="primary">
-                            Get things done
+                        <Button className="cta">
+                            Continue
                         </Button> 
                     </Link>
                 </div>
+                <img src={howItWorks} alt="how It Works" className="howItWorks" />
             </div>
-        </Layout>
+        </RandomGraphicElement>
     );
 }
 
