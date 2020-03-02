9b6a2c59d02a2b20f940aea60e49523a0cb24765
Alexey Sobolev <sobolev@mail.de>
2020-03-02 16:19:25 +0100

    feat: intro mobile page content update

--- web/src/pages/IntroShowMobile.tsx
+++ web/src/pages/IntroShowMobile.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Link } from 'react-router-dom'
 import { Button } from 'antd';
 import useStep from "../utils/useStep";
-import { Layout } from "../components";
-import mobile from '../assets/mobile.svg'
+import mobile from '../assets/mobile.png'
+import { RandomGraphicElement } from '../components'
 
 /**
  * Component which will display a IntroShowTodos.
@@ -12,24 +12,21 @@ const IntroShowTodos: React.FC = ({ match }: any) => {
     const { nextStep } = useStep(match); 
     
     return (
-        <Layout match={match}>
-            <div className="demo-intro">
-                <div className="app">
+        <RandomGraphicElement elements={7}>
+            <div className="theme-demo">
+                <div className="demo-intro">
                     <h1>That looks like a lot of work...</h1>
-                    <h3>It’s a pretty long to-do list for today, but using IOTA’s Unified Identity Protocol and the Selv app,  it will be ​quick and easy​.</h3>
-                    <div className="info">
-                        <p>This demo can be best experienced using the browser on a PC in combination with the Selv app on your phone. A full phone experience is possible using the mobile browser and the Selv app.</p>
-                        
-                        <Link to={nextStep}>
-                            <Button type="primary">
-                                Let's get started 
-                            </Button> 
-                        </Link>
-                    </div>
-                    <img className="phone" src={mobile} alt="Mobile phone" />
+                    <h3>But using IOTA’s Unified Identity Protocol and the Selv app, it will be <strong>​quick and easy​.</strong></h3>
+                    <Link to={nextStep}>
+                        <Button className="cta">
+                            Let's do it 
+                        </Button> 
+                    </Link>
                 </div>
+                <img className="phone" src={mobile} alt="Mobile phone" />
+                <p className="note">This demo can be best experienced using the browser on a PC in combination with the Selv app on your phone. A full phone experience is possible using the mobile browser and the Selv app.</p>
             </div>
-        </Layout>
+        </RandomGraphicElement>
     );
 }
 
