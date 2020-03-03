9dd25882b93f250489d5888cf23a026fccec79cf
Alexey Sobolev <sobolev@mail.de>
2020-03-03 19:16:37 +0100

    feat: updated intro todos page

--- web/src/pages/IntroShowTodos.tsx
+++ web/src/pages/IntroShowTodos.tsx
@@ -4,6 +4,7 @@ import { Button } from 'antd';
 import { RandomGraphicElement } from '../components'
 import useStep from "../utils/useStep";
 import howItWorks from '../assets/landing/howItWorks1.png' 
+import dots from '../assets/backgrounds/dots.png'
 
 /**
  * Component which will display a IntroShowTodos.
@@ -12,7 +13,14 @@ const IntroShowTodos: React.FC = ({ match }: any) => {
     const { nextStep } = useStep(match); 
     
     useEffect(() => {
-        const reset = async () => await localStorage.clear()
+        const reset = async () => {
+            window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: 'smooth'
+            });
+            await localStorage.clear()
+        }
         reset()
     }, [])
 
@@ -20,20 +28,26 @@ const IntroShowTodos: React.FC = ({ match }: any) => {
         <RandomGraphicElement elements={7}>
             <div className="theme-demo">
                 <div className="demo-intro">
-                    <h2>Welcome to the <span className="highlight">Selv demo</span></h2>
-                    <h3>Here is <strong>your to-do list</strong> for today:</h3>
-                    <ul className="todos">
-                        <li>Set up a company</li>
-                        <li>Get a bank account</li>
-                        <li>Get liability insurance</li>
-                    </ul>
-                    <Link to={nextStep}>
-                        <Button className="cta">
-                            Continue
-                        </Button> 
-                    </Link>
+                    <div className="todos">
+                        <span className="heading"><h2>Welcome to the</h2>&nbsp;&nbsp;&nbsp;<h2 className="highlight">Selv demo</h2></span>
+                        <h3>Here is <strong>your to-do list</strong> for today:</h3>
+                        <ul className="todos">
+                            <li>Set up a company</li>
+                            <li>Get a bank account</li>
+                            <li>Get liability insurance</li>
+                        </ul>
+                        <Link to={nextStep}>
+                            <Button className="cta">
+                                Continue
+                            </Button> 
+                        </Link>
+                    </div>
+                    <div className="image-wrapper">
+                        <img src={howItWorks} alt="how It Works" className="howItWorks" />
+                    </div>
+                    <img src={dots} alt="" className="dots-top" />
+                    <img src={dots} alt="" className="dots-bottom" />
                 </div>
-                <img src={howItWorks} alt="how It Works" className="howItWorks" />
             </div>
         </RandomGraphicElement>
     );
