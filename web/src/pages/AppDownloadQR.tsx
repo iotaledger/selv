e3eeb8024abab024d1d0aac04e53ae23e021ac8b
Alexey Sobolev <sobolev@mail.de>
2020-03-03 19:17:47 +0100

    feat: updated app download page

--- web/src/pages/AppDownloadQR.tsx
+++ web/src/pages/AppDownloadQR.tsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'antd';
-import { Layout, QRCode } from "../components";
+import { Layout, QRCode, RandomGraphicElement } from "../components";
 import useStep from "../utils/useStep";
 import appStore from '../assets/appStore.svg'
 import googlePlay from '../assets/googlePlay.svg'
+import avatar1 from '../assets/avatar1.png'
+import avatar2 from '../assets/avatar2.png'
+import dots from '../assets/backgrounds/dots.png'
+import circle from '../assets/backgrounds/circleFrame6.svg'
 
 /**
  * Component which will display a AppDownloadQR.
@@ -16,35 +20,48 @@ const AppDownloadQR: React.FC = ({ match }: any) => {
     const [qrContent] = useState(link);
 
     return (
-        <Layout match={match}>
-            <div className="scan-qr-page-wrapper">
-                <h1>Download the Selv app</h1>
-                <p>Scan this QR code with you camera to download the app</p>
-                <QRCode text={qrContent} />
+        <Layout match={match} noHeader noFooter>
+            <div className="scan-qr-page-wrapper app-download">
+                <RandomGraphicElement elements={5}>
+                    <React.Fragment>
+                        <h1 className="title">Download the Selv app</h1>
+                        <p className="subtitle">Press continue below when the Selv app is installed on your phone</p>
 
-                <div className="app-cta-wrapper">
-                    <a 
-                        href='https://apps.apple.com/us/app/trinity-wallet/id1385929472?ls=1'
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <img src={appStore} alt="Apple AppStore" />
-                    </a>
-                    <a 
-                        href='https://play.google.com/store/apps/details?id=com.iota.trinity'
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <img src={googlePlay} alt="Google Play Market" />
-                    </a>
-                </div>
-
-                <p>Press <strong>Continue</strong> when you have the Selv app on your phone</p>
-                <Link to={nextStep} className="cta">
-                    <Button>
-                        Continue
-                    </Button> 
-                </Link>
+                        <div className="app-cta-wrapper">
+                            <a 
+                                href='https://apps.apple.com/us/app/trinity-wallet/id1385929472?ls=1'
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img src={appStore} alt="Apple AppStore" />
+                            </a>
+                            <a 
+                                href='https://play.google.com/store/apps/details?id=com.iota.trinity'
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img src={googlePlay} alt="Google Play Market" />
+                            </a>
+                        </div>
+                        <div className="scan-wrapper">
+                            <div className="qr-content-wrapper">
+                                <p className="scan-note">Scan this QR code<br />to download</p>
+                                <div className="qr-wrapper">
+                                    <QRCode text={qrContent} size={200} />
+                                </div>
+                                <Link to={nextStep} className="cta">
+                                    <Button>
+                                        Continue
+                                    </Button> 
+                                </Link>
+                            </div>
+                            <img src={avatar1} alt="" className="avatar1" />
+                            <img src={avatar2} alt="" className="avatar2" />
+                            <img src={circle} alt="" className="circle" />
+                        </div>
+                    </React.Fragment>
+                </RandomGraphicElement>
+                <img src={dots} alt="" className="dots" />
             </div>
         </Layout>
     );
