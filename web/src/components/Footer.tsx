c099d7afc29c33fb801f0cbd9e95c4fe0e807fa6
Alexey Sobolev <sobolev@mail.de>
2020-03-06 14:17:10 +0100

    feat: code formatting

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import companyHouse from '../assets/companyHouseCrone.svg'
-import snsBank from '../assets/snsBank.svg'
+// import companyHouse from '../assets/companyHouseCrone.svg'
+// import snsBank from '../assets/snsBank.svg'
+import selv from '../assets/selv.svg';
 
 const Footer = ({ children, theme }: {
     children?: JSX.Element | null | undefined;
     theme: string | undefined;
 }) => {
     return (
-        <div className="footer-wrapper">
+        <div className='footer-wrapper'>
             {children}
-            <div className="logo">
-                { theme === 'company' && <img src={companyHouse} alt="Company House Logo" /> }
+            <div className='logo'>
+                <img src={selv} alt='' />
+                {/* { theme === 'company' && <img src={companyHouse} alt="Company House Logo" /> }
                 { theme === 'bank' && <img src={snsBank} alt="SNS Bank Logo" /> }
-                { theme === 'insurance' && <img src={snsBank} alt="SNS Bank Logo" /> }
+                { theme === 'insurance' && <img src={snsBank} alt="SNS Bank Logo" /> } */}
             </div>
         </div>
     );
 };
 
-export default Footer
+export default Footer;
