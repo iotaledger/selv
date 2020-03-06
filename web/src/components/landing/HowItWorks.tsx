68a0d04111fa18fd2ff3e9db394f736609ef7370
Alexey Sobolev <sobolev@mail.de>
2020-03-06 13:46:14 +0100

    feat: code formatting

--- web/src/components/landing/HowItWorks.tsx
+++ web/src/components/landing/HowItWorks.tsx
@@ -1,38 +1,38 @@
 import React from 'react';
-import { RandomGraphicElement } from '../'
-import selv from '../../assets/selv.svg' 
-import howItWorks1 from '../../assets/landing/howItWorks1.png' 
-import howItWorks1Mobile from '../../assets/landing/howItWorks1-mobile.png' 
-import howItWorks2 from '../../assets/landing/howItWorks2.png' 
-import howItWorks2Mobile from '../../assets/landing/howItWorks2-mobile.png' 
-import circle from '../../assets/backgrounds/circleFrame1.svg'
+import { RandomGraphicElement } from '../';
+import selv from '../../assets/selv.svg';
+import howItWorks1 from '../../assets/landing/howItWorks1.png';
+import howItWorks1Mobile from '../../assets/landing/howItWorks1-mobile.png';
+import howItWorks2 from '../../assets/landing/howItWorks2.png';
+import howItWorks2Mobile from '../../assets/landing/howItWorks2-mobile.png';
+import circle from '../../assets/backgrounds/circleFrame1.svg';
 
 export default () => (
     <RandomGraphicElement elements={10}>
-        <div className="how-it-works-section" id="how-it-works">
-            <img src={circle} alt="" className="circle" />
-            <h4 data-aos="fade-up" data-aos-duration="2000">In brief</h4>
-            <h2 data-aos="fade-up" data-aos-duration="2000">How it works</h2>
-            <div className="content-wrapper">
-                <div className="desktop">
-                    <div className="column">
+        <div className='how-it-works-section' id='how-it-works'>
+            <img src={circle} alt='' className='circle' />
+            <h4 data-aos='fade-up' data-aos-duration='2000'>In brief</h4>
+            <h2 data-aos='fade-up' data-aos-duration='2000'>How it works</h2>
+            <div className='content-wrapper'>
+                <div className='desktop'>
+                    <div className='column'>
                         <Create />
                         <Share />
-                        <img data-aos="fade-left" data-aos-duration="2000" src={howItWorks2} alt="how It Works" className="howItWorks2" />
+                        <img data-aos='fade-left' data-aos-duration='2000' src={howItWorks2} alt='how It Works' className='howItWorks2' />
                     </div>
-                    <div className="column">
-                        <img data-aos="fade-up" data-aos-duration="2000" src={howItWorks1} alt="how It Works" className="howItWorks1" />
+                    <div className='column'>
+                        <img data-aos='fade-up' data-aos-duration='2000' src={howItWorks1} alt='how It Works' className='howItWorks1' />
                         <Grow />
                         <Accept />
                     </div>
                 </div>
-                <div className="mobile">
+                <div className='mobile'>
                     <Create />
                     <Grow />
-                    <img data-aos="fade-up" data-aos-duration="2000" src={howItWorks1Mobile} alt="how It Works" className="howItWorks1" />
+                    <img data-aos='fade-up' data-aos-duration='2000' src={howItWorks1Mobile} alt='how It Works' className='howItWorks1' />
                     <Share />
                     <Accept />
-                    <img data-aos="fade-up" data-aos-duration="2000" src={howItWorks2Mobile} alt="how It Works" className="howItWorks2" />
+                    <img data-aos='fade-up' data-aos-duration='2000' src={howItWorks2Mobile} alt='how It Works' className='howItWorks2' />
                 </div>
             </div>
         </div>
@@ -40,9 +40,9 @@ export default () => (
 );
 
 const Create = () => (
-    <div className="info" data-aos="fade-left" data-aos-duration="2000">
-        <span className="content-header-wrapper">
-            <img src={selv} alt="" className="logo" />
+    <div className='info' data-aos='fade-left' data-aos-duration='2000'>
+        <span className='content-header-wrapper'>
+            <img src={selv} alt='' className='logo' />
             <h5>Create</h5>
         </span>
         <p>Download a SSI app and create a unique identifier, representing you online. This profile remains in your control and doesn’t leave the app without your consent.</p>
@@ -50,9 +50,9 @@ const Create = () => (
 );
 
 const Share = () => (
-    <div className="info" data-aos="fade-left" data-aos-duration="2000">
-        <span className="content-header-wrapper">
-            <img src={selv} alt="" className="logo" />
+    <div className='info' data-aos='fade-left' data-aos-duration='2000'>
+        <span className='content-header-wrapper'>
+            <img src={selv} alt='' className='logo' />
             <h5>Share</h5>
         </span>
         <p>Decide who and what others get to see from your personal profile. You can share your data peer-to-peer or on request. Controlled data flow allows you to always get something in return for your data.</p>
@@ -60,9 +60,9 @@ const Share = () => (
 );
 
 const Grow = () => (
-    <div className="info" data-aos="fade-right" data-aos-duration="2000">
-        <span className="content-header-wrapper">
-            <img src={selv} alt="" className="logo" />
+    <div className='info' data-aos='fade-right' data-aos-duration='2000'>
+        <span className='content-header-wrapper'>
+            <img src={selv} alt='' className='logo' />
             <h5>Grow</h5>
         </span>
         <p>Gather statements, or credentials, about yourself, verified by others, to grow your personal identity. Fully under your control and not stored in any database.</p>
@@ -70,9 +70,9 @@ const Grow = () => (
 );
 
 const Accept = () => (
-    <div className="info" data-aos="fade-right" data-aos-duration="2000">
-        <span className="content-header-wrapper">
-            <img src={selv} alt="" className="logo" />
+    <div className='info' data-aos='fade-right' data-aos-duration='2000'>
+        <span className='content-header-wrapper'>
+            <img src={selv} alt='' className='logo' />
             <h5>Accept</h5>
         </span>
         <p>By accepting BYOI, companies can verify customer data, instantly and for free. This allows you to speed up processes such as onboarding and reduce the amount of typing your customers have to do.</p>
--- web/src/components/landing/Main.tsx
+++ web/src/components/landing/Main.tsx
@@ -1,63 +1,62 @@
 import React from 'react';
-import { RandomGraphicElement } from '../'
-import main from '../../assets/landing/main.png' 
-import appStore from '../../assets/appStore.svg'
-import googlePlay from '../../assets/googlePlay.svg'
-import dots from '../../assets/backgrounds/dots.png'
-import ellipse from '../../assets/backgrounds/ellipse1.svg'
+import { RandomGraphicElement } from '../';
+import main from '../../assets/landing/main.png';
+import appStore from '../../assets/appStore.svg';
+import googlePlay from '../../assets/googlePlay.svg';
+import dots from '../../assets/backgrounds/dots.png';
+import ellipse from '../../assets/backgrounds/ellipse1.svg';
 
 export default () => {
     return (
         <RandomGraphicElement elements={5}>
-            <div className="heading-section">
-                <div className="desktop">
-                    <div className="content-wrapper">
-                        <div className="content">
+            <div className='heading-section'>
+                <div className='desktop'>
+                    <div className='content-wrapper'>
+                        <div className='content'>
                             <Content />
                             <Buttons />
                         </div>
                     </div>
-                    <div className="image-wrapper">
-                        <img src={main} alt="Portrait" className="portrait" />
+                    <div className='image-wrapper'>
+                        <img src={main} alt='Portrait' className='portrait' />
                     </div>
-                    <img src={dots} alt="" className="dots" />
-                    <img src={ellipse} alt="" className="ellipse" />
+                    <img src={dots} alt='' className='dots' />
+                    <img src={ellipse} alt='' className='ellipse' />
                 </div>
-                <div className="mobile">
-                    <div className="content">
+                <div className='mobile'>
+                    <div className='content'>
                         <Content />
                     </div>
-                    <img src={main} alt="Portrait" className="portrait" />
+                    <img src={main} alt='Portrait' className='portrait' />
                     <Buttons />
                 </div>
             </div>
         </RandomGraphicElement>
     );
-}
+};
 
 const Content = () => (
-    <React.Fragment>
+    <>
         <h1>Claim, Control &<br />Reuse your new<br />Digital Identity</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-    </React.Fragment>
+    </>
 );
 
-
 const Buttons = () => (
-    <div className="store-buttons">
-        <a 
+    <div className='store-buttons'>
+        <a
             href='https://apps.apple.com/us/app/trinity-wallet/id1385929472?ls=1'
-            target="_blank"
-            rel="noopener noreferrer"
+            target='_blank'
+            rel='noopener noreferrer'
         >
-            <img src={appStore} alt="Apple AppStore" />
+            <img src={appStore} alt='Apple AppStore' />
         </a>
-        <a 
+        <a
             href='https://play.google.com/store/apps/details?id=com.iota.trinity'
-            target="_blank"
-            rel="noopener noreferrer"
+            target='_blank'
+            rel='noopener noreferrer'
         >
-            <img src={googlePlay} alt="Google Play Market" />
+            <img src={googlePlay} alt='Google Play Market' />
         </a>
     </div>
 );
