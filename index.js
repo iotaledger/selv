d5a3725bcb46393fd15d20143e36e021d0431b97
Alexey Sobolev <sobolev@mail.de>
2020-04-08 22:02:53 +0200

    server side auth

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const protect = require('static-auth')
-const route = '/'
-const { USERNAME, PASSWORD } = process.env
-const isAuthenticated = (user, pass) => (user === USERNAME && pass === PASSWORD)
+const protect = require('static-auth');
+
+const route = '/';
+const { USERNAME, PASSWORD } = process.env;
+const isAuthenticated = (user, pass) => (user === USERNAME && pass === PASSWORD);
 const options = {
-  directory: __dirname + '/public'
+  directory: __dirname + '/build'
+};
+
+const app = protect(route, isAuthenticated, options);
+
+module.exports = app;
+
+
+// Serve on localhost if asked to.
+// NOTE: This only used for testing locally, this is NOT needed nor used by Now once deployed.
+if (process.env.SERVE === 'true') {
+  const http = require('http');
+  const server = http.createServer(app);
+  server.listen(4444, () => console.log('Listening on port 4444...'));
 }
-const app = protect(route, isAuthenticated, options)
-module.exports = app
