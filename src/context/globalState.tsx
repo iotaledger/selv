100c2479a7a416c525f50f2c4fdf98796edce4a7
Alexey Sobolev <sobolev@mail.de>
2020-04-15 20:18:00 +0200

    feat: updated context

--- src/context/globalState.tsx
+++ src/context/globalState.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppContext from './app-context';
 import { routes, mainSteps } from '../steps';
 
 const GlobalState = ({ children }: any) => {
+    const [requestPassword, setRequestPassword] = useState(false);
+
     return (
-        <AppContext.Provider value={{ mainSteps, routes }}>
+        <AppContext.Provider value={{
+            mainSteps, routes, requestPassword, setRequestPassword 
+        }}>
             {children}
         </AppContext.Provider>
     );
