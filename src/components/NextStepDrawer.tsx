7b1549f7685d18222091a15e3c29c79241ad4e05
Alexey Sobolev <sobolev@mail.de>
2020-04-15 20:22:10 +0200

    feat: updated next step drawer

--- src/components/NextStepDrawer.tsx
+++ src/components/NextStepDrawer.tsx
@@ -3,72 +3,34 @@ import { Link } from 'react-router-dom';
 import { Button } from 'antd';
 
 export default ({ link }: { link: string }) => {
-    const [nextStep, setNextStep] = useState('');
+    const [show, setShow] = useState(false);
 
     useEffect(() => {
         async function setInfo () {
-            const companyHouse = await localStorage.getItem('companyHouse');
-            const bank = await localStorage.getItem('bank');
-            const insurance = await localStorage.getItem('insurance');
-            if (insurance && insurance === 'completed') {
-                setNextStep('completed');
-            } else if (bank && bank === 'completed') {
-                setNextStep('insurance');
-            } else if (companyHouse && companyHouse === 'completed') {
-                setNextStep('bank');
+            const healthAuthorityStatus = await localStorage.getItem('healthAuthority');
+
+            if (!healthAuthorityStatus || healthAuthorityStatus !== 'completed') {
+                setShow(true);
             }
         }
         setInfo();
     }, []);
 
-    if (!link) {
+    if (!show || !link) {
         return <React.Fragment />;
     }
 
-    switch (nextStep) {
-    case 'completed':
-        return (
-            <div className='next-step-drawer completed'>
-                <h3>Your company is now Active</h3>
-                <p>
-                        You have successfully set up your entire business. This is the end of this demo and we thank you for participating. Selv will be updated with more demonstrations and will be updated with more features
-                </p>
-                <Link to={link}>
-                    <Button>
-                            Finish demo
-                    </Button>
-                </Link>
-            </div>
-        );
-    case 'insurance':
-        return (
-            <div className='next-step-drawer'>
-                <h3>Your company is not Active yet</h3>
-                <p>
-                        You need to add Liability Insurance to finish this process
-                </p>
-                <Link to={link}>
-                    <Button>
-                            Get insurance
-                    </Button>
-                </Link>
-            </div>
-        );
-    case 'bank':
-        return (
-            <div className='next-step-drawer'>
-                <h3>Your company is not Active yet</h3>
-                <p>
-                        You need to add Bank Account and Liability Insurance to finish this process
-                </p>
-                <Link to={link}>
-                    <Button>
-                            Open bank account
-                    </Button>
-                </Link>
-            </div>
-        );
-    default:
-        return <React.Fragment />;
-    }
+    return (
+        <div className='next-step-drawer'>
+            <h3>You have not added your credential to Selv App</h3>
+            <p>
+                Download your immunity certificate
+            </p>
+            <Link to={link}>
+                <Button>
+                    Continue
+                </Button>
+            </Link>
+        </div>
+    );
 };
