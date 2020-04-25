1104888b8ec4aa2f33e559734a4a3739af9f011f
Charlie Varley <charlie.varley@iota.org>
2020-04-26 00:14:04 +0100

    fix: remove qr logo

--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import QRCode, { ImageSettings } from 'qrcode.react';
-import logo from '../assets/iota.png';
 
 export default ({ text, size }: { text: string; size?: number; }) => {
-    const imageSettings: ImageSettings = {
-        src: logo,
-        height: 55,
-        width: 55,
-        excavate: true
-    };
 
     return (
         <div className='qr-code'>
@@ -17,7 +10,6 @@ export default ({ text, size }: { text: string; size?: number; }) => {
                     value={text}
                     size={size || 290}
                     level='H'
-                    imageSettings={imageSettings}
                 />
             )}
         </div>
