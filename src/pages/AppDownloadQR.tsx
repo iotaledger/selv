dc62cc349d8e08b8c22b20e67d7447c3322ed55d
Charlie Varley <charlie.varley@iota.org>
2020-05-23 23:50:24 +0100

    fix: qr route

--- src/pages/AppDownloadQR.tsx
+++ src/pages/AppDownloadQR.tsx
@@ -15,7 +15,7 @@ import circle from '../assets/backgrounds/circleFrame6.svg';
  */
 const AppDownloadQR: React.FC = ({ match }: any) => {
     const { nextStep } = useStep(match);
-    const [qrContent] = useState(window.location.host + '/qr-redirect');
+    const [qrContent] = useState(window.location.origin + '/qr-redirect');
 
     return (
         <Layout match={match} noHeader>
