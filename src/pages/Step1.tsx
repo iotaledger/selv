7107be8b9630fe9439242e890626a9523cc69155
Charlie Varley <charlie.varley@iota.org>
2020-05-25 00:22:42 +0100

    fix: update text

--- src/pages/Step1.tsx
+++ src/pages/Step1.tsx
@@ -33,7 +33,7 @@ const Step1: React.FC = ({ match }: any) => {
                                     </span>
                                     <p>You have recently taken a test that shows you do not pose an infection risk to others.</p>
                                     <br />
-                                    <p>Your National Health Authority has alerted you to collect your digital health credential from their portal.</p>
+                                    <p>Your digital health credential is now available from your national health authority’s online portal.</p>
                                 </div>
                             </div>
                         </div>
