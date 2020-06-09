2dd9295d58f9773bf8369c826025f6a47ef4c8e2
Alexey Sobolev <sobolev@mail.de>
2020-06-09 20:16:25 +0200

    feat: code formatting

--- web/api/routes/index.js
+++ web/api/routes/index.js
@@ -1,16 +1,16 @@
 try {
-    const server = require('../lib/server')
+    const server = require('../lib/server');
   
     server.get('/qr-redirect', async (req, res) => {
-        var ua = req.header('user-agent');
-        if (/iphone|ipod|ipad/i.test(ua)){
-            res.redirect('https://testflight.apple.com/join/3FCosIcj')
+        const ua = req.header('user-agent');
+        if (/iphone|ipod|ipad/i.test(ua)) {
+            res.redirect('https://testflight.apple.com/join/3FCosIcj');
         } else {
-            res.redirect('https://play.google.com/apps/testing/com.iota.selv.demo')
+            res.redirect('https://play.google.com/apps/testing/com.iota.selv.demo');
         }
-    })
+    });
   
-    module.exports = server
+    module.exports = server;
 } catch (error) {
-    console.log('API Error', error)
+    console.log('API Error', error);
 }
