761d846c101323d3e8eb4a6f02108dd775b5501b
Alexey Sobolev <sobolev@mail.de>
2020-06-10 15:23:31 +0200

    fix: cleanup

--- src/context/globalState.tsx
+++ src/context/globalState.tsx
@@ -1,16 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import AppContext from './app-context';
 import { routes, mainSteps } from '../steps';
 
-const GlobalState = ({ children }: any) => {
-
-    return (
-        <AppContext.Provider value={{
-            mainSteps, routes
-        }}>
-            {children}
-        </AppContext.Provider>
-    );
-};
+const GlobalState = ({ children }: any) => (
+    <AppContext.Provider value={{ mainSteps, routes }}>
+        {children}
+    </AppContext.Provider>
+);
 
 export default GlobalState;
