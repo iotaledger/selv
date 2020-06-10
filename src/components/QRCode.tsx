0ba9c3552000085241e8e2f7d1ce417c7e7c2157
Alexey Sobolev <sobolev@mail.de>
2020-06-10 15:23:23 +0200

    fix: cleanup

--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import QRCode, { ImageSettings } from 'qrcode.react';
+import QRCode from 'qrcode.react';
 
-export default ({ text, size }: { text: string; size?: number; }) => {
-
-    return (
-        <div className='qr-code'>
-            { text && (
-                <QRCode
-                    value={text}
-                    size={size || 290}
-                    level='H'
-                />
-            )}
-        </div>
-    );
-};
+export default ({ text, size }: { text: string; size?: number; }) => (
+    <div className='qr-code'>
+        { text && (
+            <QRCode
+                value={text}
+                size={size || 290}
+                level='H'
+            />
+        )}
+    </div>
+);
