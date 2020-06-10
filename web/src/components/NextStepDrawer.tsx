4b3eb2171581da34c784e04ad07d5ba19181212f
Alexey Sobolev <sobolev@mail.de>
2020-06-10 14:13:07 +0200

    feat: code formatting

--- web/src/components/NextStepDrawer.tsx
+++ web/src/components/NextStepDrawer.tsx
@@ -26,49 +26,49 @@ export default ({ link }: { link: string }) => {
     }
 
     switch (nextStep) {
-    case 'completed':
-        return (
-            <div className='next-step-drawer completed'>
-                <h3>Your company is now Active</h3>
-                <p>
+        case 'completed':
+            return (
+                <div className='next-step-drawer completed'>
+                    <h3>Your company is now Active</h3>
+                    <p>
                         You have successfully set up your entire business. This is the end of this demo and we thank you for participating. Selv will be updated with more demonstrations and will be updated with more features
-                </p>
-                <Link to={link}>
-                    <Button>
-                            Finish demo
-                    </Button>
-                </Link>
-            </div>
-        );
-    case 'insurance':
-        return (
-            <div className='next-step-drawer'>
-                <h3>Your company is not Active yet</h3>
-                <p>
+                    </p>
+                    <Link to={link}>
+                        <Button>
+                                Finish demo
+                        </Button>
+                    </Link>
+                </div>
+            );
+        case 'insurance':
+            return (
+                <div className='next-step-drawer'>
+                    <h3>Your company is not Active yet</h3>
+                    <p>
                         You need to add Liability Insurance to finish this process
-                </p>
-                <Link to={link}>
-                    <Button>
+                    </p>
+                    <Link to={link}>
+                        <Button>
                             Get insurance
-                    </Button>
-                </Link>
-            </div>
-        );
-    case 'bank':
-        return (
-            <div className='next-step-drawer'>
-                <h3>Your company is not Active yet</h3>
-                <p>
+                        </Button>
+                    </Link>
+                </div>
+            );
+        case 'bank':
+            return (
+                <div className='next-step-drawer'>
+                    <h3>Your company is not Active yet</h3>
+                    <p>
                         You need to add Bank Account and Liability Insurance to finish this process
-                </p>
-                <Link to={link}>
-                    <Button>
+                    </p>
+                    <Link to={link}>
+                        <Button>
                             Open bank account
-                    </Button>
-                </Link>
-            </div>
-        );
-    default:
-        return <React.Fragment />;
+                        </Button>
+                    </Link>
+                </div>
+            );
+        default:
+            return <React.Fragment />;
     }
 };
