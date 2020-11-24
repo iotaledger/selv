c78f3b750b1216b44b82400fd136a068d21d1963
Alexey Sobolev <sobolev@mail.de>
2020-11-24 14:14:39 +0100

    Header & footer

--- web/persistent-selves/src/components/Footer.js
+++ web/persistent-selves/src/components/Footer.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import companyHouse from '../assets/companyHouseCrone.svg'
-import snsBank from '../assets/snsBank.svg'
 
-const Footer = ({ children, theme }) => {
+const Footer = ({ children }) => {
     return (
         <div className='footer-wrapper' id='footer'>
             {children}
-            <div className='logo'>
-                { theme === 'company' && <img src={companyHouse} alt='Company House Logo' /> }
-                { theme === 'bank' && <img src={snsBank} alt='SNS Bank Logo' /> }
-                { theme === 'insurance' && <img src={snsBank} alt='SNS Bank Logo' /> }
-            </div>
         </div>
     );
 };
--- web/persistent-selves/src/components/Header.js
+++ web/persistent-selves/src/components/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import companyHouse from '../assets/companyHouse.svg'
-import snsBank from '../assets/snsBank.svg'
+import registry from '../assets/registry.svg'
+import future from '../assets/futureFoundation.svg'
+import present from '../assets/presentFoundation.svg'
 
 const Header = ({ children, theme }) => {
     return (
         <div className='header-wrapper'>
             <div className='logo'>
-                { theme === 'company' && <img src={companyHouse} alt='Company House Logo' /> }
-                { theme === 'bank' && <img src={snsBank} alt='SNS Bank Logo' /> }
-                { theme === 'insurance' && <img src={snsBank} alt='SNS Bank Logo' /> }
+                { theme === 'registry' && <img src={registry} alt='Good Ancestor Registry' /> }
+                { theme === 'future' && <img src={future} alt='Far Future Foundation' /> }
+                { theme === 'present' && <img src={present} alt='Act Right Now Foundation' /> }
             </div>
             { children }
         </div>
