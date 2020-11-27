6833efe63b4a235186240a5cf65429fd92f387d5
Alexey Sobolev <sobolev@mail.de>
2020-11-27 17:22:11 +0100

    Personalize commitments page

--- web/persistent-selves/src/pages/PersonalizeCommitments.js
+++ web/persistent-selves/src/pages/PersonalizeCommitments.js
@@ -12,42 +12,23 @@ import image from '../assets/greatSuccess/image1.png';
 const PersonalizeCommitments = ({ history, match }) => {
     const { nextStep } = useStep(match);
 
-    useEffect(() => {
-        async function getData () {
-            const credentialsString = await localStorage.getItem('credentials');
-            const credentials = credentialsString && await JSON.parse(credentialsString);
-            const status = credentials?.status;
-            if (!status || Number(status) !== 2) {
-                history.goBack();
-            }
-        }
-        getData();
-    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    const commitments = history?.location?.state?.commitments;
+    console.log(444, commitments)
+    // useEffect(() => {
+    //     async function getData () {
+    //         const credentialsString = await localStorage.getItem('credentials');
+    //         const credentials = credentialsString && await JSON.parse(credentialsString);
+    //         const status = credentials?.status;
+    //         if (!status || Number(status) !== 2) {
+    //             history.goBack();
+    //         }
+    //     }
+    //     getData();
+    // }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
     return (
         <Layout match={match}>
-            <RandomGraphicElement elements={5}>
-                <div className='sign-in-confirmation'>
-                    <div className='sign-in-confirmation-logo-wrapper'>
-                        <img src={selv} alt='Selv app logo' />
-                        <h2>Access Authorised</h2>
-                    </div>
-                    <div className='sign-in-confirmation-content-wrapper'>
-                        <img className='image' src={image} alt='Access Authorised' />
-                        <div className='sign-in-confirmation-content'>
-                            <h2>You have now signed in to the registry without ever creating an account.</h2>
-                            <p>
-                                No more need for endless account and password creations. Verifying your data is nearly instant and completely free.
-                            </p>
-                            <Link to={nextStep}>
-                                <Button>
-                                    Continue
-                                </Button>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            </RandomGraphicElement>
+
         </Layout>
     );
 };
