6c070ab5706ed2362fe89385b2e4f579835cb03e
Alexey Sobolev <sobolev@mail.de>
2020-11-27 17:21:50 +0100

    Select commitments category page

--- web/persistent-selves/src/pages/SelectCommitmentCategory.js
+++ web/persistent-selves/src/pages/SelectCommitmentCategory.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'antd';
+import { Button, Card } from 'antd';
 import { Layout } from '../components';
 import future from '../assets/futureCategory.svg';
 import present from '../assets/presentCategory.svg';
@@ -12,22 +12,26 @@ import arrow from '../assets/arrow.svg';
 const SelectCommitmentCategory = ({ history, match }) => {
     const [disabled, updateDisabled] = useState('disabled');
 
-    useEffect(() => {
-        async function getData () {
-            const credentialsString = await localStorage.getItem('credentials');
-            const credentials = credentialsString && await JSON.parse(credentialsString);
-            const status = credentials?.status;
-            if (!status || Number(status) !== 2) {
-                history.goBack();
-            }
-        }
-        getData();
-    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    // useEffect(() => {
+    //     async function getData () {
+    //         const credentialsString = await localStorage.getItem('credentials');
+    //         const credentials = credentialsString && await JSON.parse(credentialsString);
+    //         const status = credentials?.status;
+    //         if (!status || Number(status) !== 2) {
+    //             history.goBack();
+    //         }
+    //     }
+    //     getData();
+    // }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
     return (
         <Layout match={match}>
             <div className='select-commitment-category-wrapper'>
-                <div className='commitment-category'>
+                <Card 
+                    bordered
+                    hoverable 
+                    className='commitment-category'
+                >
                     <div className='commitment-category-image-wrapper'>
                         <img className='commitment-category-image' src={future} alt='Far Future Foundation' />
                         <h2>Far Future Foundation</h2>
@@ -37,15 +41,22 @@ const SelectCommitmentCategory = ({ history, match }) => {
                         <p>
                             The far future commitment allows you to make conditional choices about a far-away future based on oracles and smart contracts.
                         </p>
-                        <Link to='/future/select/1'>
+                        <Link to={{
+                            pathname: '/future/select/1',
+                            state: { category: 'future' }
+                        }}>
                             <Button className='category-future'>
                                 Visit Now
                                 <img className='arrow' src={arrow} alt='' />
                             </Button>
                         </Link>
                     </div>
-                </div>
-                <div className={`commitment-category ${disabled}`}>
+                </Card>
+                <Card 
+                    bordered
+                    hoverable={!disabled} 
+                    className={`commitment-category ${disabled}`}
+                >
                     <div className='commitment-category-image-wrapper'>
                         <img className='commitment-category-image' src={present} alt='Act Right Now Foundation' />
                         <h2>Act Right Now Foundation</h2>
@@ -55,14 +66,17 @@ const SelectCommitmentCategory = ({ history, match }) => {
                         <p>
                             The present commitment let´s you make pledges to get involved and shaping the future yourself.
                         </p>
-                        <Link to='/present/select/2'>
-                            <Button className='category-future'>
+                        <Link to={{
+                            pathname: '/present/select/2',
+                            state: { category: 'present' }
+                        }}>
+                            <Button disabled={disabled} className='category-future'>
                                 Visit Now
                                 <img className='arrow' src={arrow} alt='' />
                             </Button>
                         </Link>
                     </div>
-                </div>
+                </Card>
             </div>
         </Layout>
     );
