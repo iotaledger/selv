a3c37863f718e5bc377333d58f9fa47e36a9d086
Alexey Sobolev <sobolev@mail.de>
2020-11-27 17:21:25 +0100

    Select commitments page

--- web/persistent-selves/src/pages/SelectCommitments.js
+++ web/persistent-selves/src/pages/SelectCommitments.js
@@ -1,53 +1,70 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'antd';
+import { Checkbox, Card } from 'antd';
 import useStep from '../utils/useStep';
 import { Layout, RandomGraphicElement } from '../components';
 import selv from '../assets/selvBordered.svg';
 import image from '../assets/greatSuccess/image1.png';
+import commitments from '../assets/commitments';
 
 /**
  * Component which will display a SelectCommitments.
  */
 const SelectCommitments = ({ history, match }) => {
+    
     const { nextStep } = useStep(match);
+    const category = history?.location?.state?.category;
+    const commitmentObject = commitments[category];
+    console.log(222, category, commitmentObject);
 
-    useEffect(() => {
-        async function getData () {
-            const credentialsString = await localStorage.getItem('credentials');
-            const credentials = credentialsString && await JSON.parse(credentialsString);
-            const status = credentials?.status;
-            if (!status || Number(status) !== 2) {
-                history.goBack();
-            }
-        }
-        getData();
-    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    // useEffect(() => {
+    //     async function getData () {
+    //         const credentialsString = await localStorage.getItem('credentials');
+    //         const credentials = credentialsString && await JSON.parse(credentialsString);
+    //         const status = credentials?.status;
+    //         if (!status || Number(status) !== 2) {
+    //             history.goBack();
+    //         }
+    //     }
+    //     getData();
+    // }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
     return (
         <Layout match={match}>
-            <RandomGraphicElement elements={5}>
-                <div className='sign-in-confirmation'>
-                    <div className='sign-in-confirmation-logo-wrapper'>
-                        <img src={selv} alt='Selv app logo' />
-                        <h2>Access Authorised</h2>
-                    </div>
-                    <div className='sign-in-confirmation-content-wrapper'>
-                        <img className='image' src={image} alt='Access Authorised' />
-                        <div className='sign-in-confirmation-content'>
-                            <h2>You have now signed in to the registry without ever creating an account.</h2>
-                            <p>
-                                No more need for endless account and password creations. Verifying your data is nearly instant and completely free.
-                            </p>
-                            <Link to={nextStep}>
-                                <Button>
-                                    Continue
-                                </Button>
-                            </Link>
-                        </div>
-                    </div>
+            <div className='select-commitment-wrapper'>
+                <div className='text-wrapper'>
+                    <h2>{commitmentObject?.title}</h2>
+                    <p>{commitmentObject?.description}</p>
                 </div>
-            </RandomGraphicElement>
+                <div className='commitments-list'>
+                    {
+                        commitmentObject?.commitments?.map(commitment => (
+                            <Card 
+                                bordered
+                                hoverable 
+                                className='commitment-item'
+                                key={commitment?.commitmentId}
+                            >
+                                <div className='commitment-image-wrapper'>
+                                    <img 
+                                        className='commitment-image' 
+                                        src={commitment?.image} 
+                                        alt={commitment?.title}
+                                    />
+                                </div>
+                                <div className='commitment-content'>
+                                    <h2>{commitment?.title}</h2>
+                                    <p>{commitment?.description}</p>
+                                   
+                                    <Checkbox className='category-future'>
+                                        Visit Now
+                                    </Checkbox>
+                                </div>
+                            </Card>
+                        ))
+                    }
+                </div>
+            </div>
         </Layout>
     );
 };
