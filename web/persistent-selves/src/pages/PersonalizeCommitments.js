a8a5ab8a84edfab514cf59da092f279c4e959b7a
Alexey Sobolev <sobolev@mail.de>
2020-11-27 18:14:34 +0100

    Personalize commitments page

--- web/persistent-selves/src/pages/PersonalizeCommitments.js
+++ web/persistent-selves/src/pages/PersonalizeCommitments.js
@@ -1,19 +1,41 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'antd';
+import { Button, Select, Card, Slider } from 'antd';
 import useStep from '../utils/useStep';
-import { Layout, RandomGraphicElement } from '../components';
-import selv from '../assets/selvBordered.svg';
-import image from '../assets/greatSuccess/image1.png';
+import { Layout } from '../components';
+import commitments from '../assets/commitments';
 
 /**
  * Component which will display a PersonalizeCommitments.
  */
+
+const { Option } = Select;
+const percentages = Array.from({ length: 11 }, (x, i) => i * 10);
+
 const PersonalizeCommitments = ({ history, match }) => {
     const { nextStep } = useStep(match);
+    const [percentage, updatePercentage] = useState([50, 50]);
 
-    const commitments = history?.location?.state?.commitments;
-    console.log(444, commitments)
+    const category = history?.location?.state?.category;
+    const selectedCommitments = history?.location?.state?.commitments;
+    const selected = commitments[category]?.commitments?.filter(commitment =>
+        selectedCommitments.includes(commitment?.commitmentId)
+    )
+
+    const marks = {
+        0: selected[0]?.title,
+        100: selected[1]?.title,
+    };
+
+    const onChange = value => updatePercentage([value, 100 - value])
+    
+    const handleConditionChange = (id, value) => {
+        console.log('handleConditionChange', id, value)
+    }
+
+    const handleSupportChange = (id, value) => {
+        console.log('handleSupportChange', id, value)
+    }
     // useEffect(() => {
     //     async function getData () {
     //         const credentialsString = await localStorage.getItem('credentials');
@@ -28,7 +50,79 @@ const PersonalizeCommitments = ({ history, match }) => {
 
     return (
         <Layout match={match}>
-
+            <React.Fragment>
+                <div className='personalize-commitment-wrapper'>
+                    <div className='text-wrapper'>
+                        <h2>Personalise your commitment</h2>
+                        <p>
+                            Choose from a number of future commitments you wish to support. Future generations will have to deal with a number of environmental issues if we continue 'business as usual'. 
+                            <br /><br /><br />
+                            You can choose two pledges to help the future as part of your legacy.
+                        </p>
+                    </div>
+                    <div className='commitments-list'>
+                        {
+                            selected?.map((commitment, index) => (
+                                <div 
+                                    className='commitment-item' 
+                                    key={commitment?.commitmentId}
+                                >
+                                    <h4>{commitment?.title}</h4>
+                                    <Card 
+                                        bordered
+                                        hoverable={false}
+                                        className='commitment-card'
+                                    >
+                                        <div className='commitment-content'>
+                                            <div>
+                                                <p>{commitment?.condition?.if}</p>
+                                                <Select 
+                                                    defaultValue={`${index === 0 ? percentages[3] : percentages[1]}%`} 
+                                                    onChange={value => handleConditionChange(commitment?.commitmentId, value)}
+                                                >
+                                                    {percentages?.map(item => (
+                                                        <Option key={item}>{item}%</Option>
+                                                    ))}
+                                                </Select>
+                                            </div>
+                                            <div><p>THEN donate {percentage[index]}% of my wallet balance</p></div>
+                                            <div>
+                                                <p>TO support</p>
+                                                <Select 
+                                                    defaultValue={commitment?.condition?.support?.[0]} 
+                                                    onChange={value => handleSupportChange(commitment?.commitmentId, value)}
+                                                >
+                                                    {commitment?.condition?.support?.map(item => (
+                                                        <Option key={item}>{item}</Option>
+                                                    ))}
+                                                </Select>
+                                            </div>
+                                        </div>
+                                    </Card>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+                <div className='commitments-drawer'>
+                    <h2>Choose your commitment level</h2>
+                    <Slider 
+                        marks={marks} 
+                        step={5} 
+                        defaultValue={50} 
+                        tooltipVisible={false}
+                        onChange={onChange}
+                    />
+                    <p>{percentage[0]}%</p>
+                    <br/>
+                    <p>{percentage[1]}%</p>
+                    <Link to={nextStep}>
+                        <Button>
+                            Continue
+                        </Button>
+                    </Link>
+                </div>
+            </React.Fragment>
         </Layout>
     );
 };
