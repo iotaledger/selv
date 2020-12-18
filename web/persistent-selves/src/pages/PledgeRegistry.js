67d07ce224291922124e0a273fd6ee4a1b1b4b68
Alexey Sobolev <sobolev@mail.de>
2020-12-18 20:35:51 +0100

    Registry page

--- web/persistent-selves/src/pages/PledgeRegistry.js
+++ web/persistent-selves/src/pages/PledgeRegistry.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { Button } from 'antd';
 import useStep from '../utils/useStep';
 import { Layout, FAQ, PledgeCards } from '../components';
-import image1 from '../assets/greatSuccess/image1.png';
+import pledgeRegistry from '../assets/pledgeRegistry.svg';
 
 /**
  * Component which will display a PledgeRegistry.
@@ -16,30 +16,40 @@ const PledgeRegistry = ({ history, match, ...props }) => {
 	return (
 		<Layout match={match} noFooter>
 			<div className={`registry-page-wrapper ${drawer}`}>
-				<div className='create-pledge-header-cta'>
-					<div className='figure-wrapper'>
-						<img className='figure' src={image1} alt='You signed in with DID' />
-					</div>
-					<div className='text-wrapper'>
-						<h2>
-							Creating the future for <br /> generations... today
-						</h2>
-						<Link to={nextStep}>
-							<Button>Create a pledge</Button>
-						</Link>
+				<div className='create-pledge-header-cta-wrapper'>
+					<div className='create-pledge-header-cta'>
+						<div className='figure-wrapper'>
+							<img className='figure' src={pledgeRegistry} alt='You signed in with DID' />
+						</div>
+						<div className='text-wrapper'>
+							<h1>
+								Your legacy is now
+							</h1>
+							<br />
+							<p>
+								Throughout our lives, we generate environmental footprints that become the legacies we pass down to future generations. Good Ancestor Registry supports individuals in building a positive environmental inheritance for our descendants.
+							</p>
+							<br />
+							<Link to={nextStep}>
+								<Button>Create a commitment</Button>
+							</Link>
+						</div>
 					</div>
 				</div>
-				<div className='registry-cta-wrapper'>
-					<h4>Future outcomes that you can impact today</h4>
-					<div className='content-wrapper'>
-						<h4>Environmental Ceiling</h4>
-					</div>
-					<PledgeCards />
+				<div className='registry-content-wrapper'>
+					<h2>Learn about the Earth’s Boundaries</h2>
+					<p>
+						The actions of past generations have created a fragile environment. We are experiencing the negative effects of those legacies. Our future and the next generations’ future depends on the health of the Earth System and its boundaries. By learning and committing to support our planet’s system today, we can start shaping a positive legacy for the future.
+					</p>
 				</div>
+				<PledgeCards />
 				<FAQ />
 				<div className='create-pledge-footer-cta'>
+					<h3>
+						Your legacy is now – Create a commitment
+					</h3>
 					<Link to={nextStep}>
-						<Button>Create a pledge</Button>
+						<Button>Create a commitment</Button>
 					</Link>
 				</div>
 			</div>
