461ccb4c7c082831d96a51c163de218b5aa7f154
Alexey Sobolev <sobolev@mail.de>
2020-12-31 17:00:36 +0100

    LifeSpan animation

---
+++ web/persistent-selves/src/animation/App/AppStateProvider/index.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useReducer } from "react";
+
+import initState from "./init-state";
+import reducer from "./reducer";
+
+export const AppDispatchContext = createContext(() => {});
+export const AppStateContext = createContext(initState);
+
+function AppStateProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initState);
+
+  return (
+    <AppDispatchContext.Provider value={dispatch}>
+      <AppStateContext.Provider value={state}>
+        {children}
+      </AppStateContext.Provider>
+    </AppDispatchContext.Provider>
+  );
+}
+
+export default AppStateProvider;
