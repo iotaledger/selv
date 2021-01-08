1c5a6d568310c2ff648f3de6e9d92aec107a6bec
RubensGitHub <44226748+RubensGitHub@users.noreply.github.com>
2021-01-08 19:24:29 +0100

    about 50% progress of adding landing page translation support

--- web/bank/src/components/landing/App.tsx
+++ web/bank/src/components/landing/App.tsx
@@ -6,7 +6,6 @@ import selv from '../../assets/selv.svg';
 import app from '../../assets/landing/app.png';
 import appMobile from '../../assets/landing/app-mobile.png';
 import circle from '../../assets/backgrounds/circleFrame2.svg';
-import { Trans } from 'react-i18next';
 import { Translation } from 'react-i18next';
 
 
@@ -19,7 +18,7 @@ export default () => (
                     <div className='app-section' id='the-app'>
                         <img src={circle} alt='' className='circle' />
                         <h4 data-aos='fade-up' data-aos-duration='1000'>{t("general.Demo")}</h4>
-                        <h2 data-aos='fade-up' data-aos-duration='1000'>The App</h2>
+                        <h2 data-aos='fade-up' data-aos-duration='1000'>{t("general.The App")}</h2>
 
                         <div className='content-wrapper'>
                             <img data-aos='fade-right' data-aos-duration='1000' src={app} alt='App' className='desktop' />
@@ -28,30 +27,30 @@ export default () => (
                                 <div className='content-item' data-aos='fade-left' data-aos-duration='1000'>
                                     <span className='content-header-wrapper'>
                                         <img src={selv} alt='' className='logo' />
-                                        <h5>Self Sovereign Identity</h5>
+                                        <h5>{t("general.Self Sovereign Identity")}</h5>
                                     </span>
-                                    <p>Through Self Sovereign Identity, people, organisations and things are empowered to collect and share their own verified data and digital identity.</p>
+                                    <p>{t("general.What it is")}</p>
                                 </div>
                                 <div className='content-item' data-aos='fade-left' data-aos-duration='1000'>
                                     <span className='content-header-wrapper'>
                                         <img src={selv} alt='' className='logo' />
-                                        <h5>One App – One Protocol</h5>
+                                        <h5>{t("views.landing.app.One App – One Protocol")}</h5>
                                     </span>
-                                    <p>This solution provides an easy and cheap way to comply with GDPR and similar privacy laws.</p>
+                                    <p>{t("views.landing.app.Easy GDPR compliance")}</p>
                                 </div>
                                 <div className='content-item' data-aos='fade-left' data-aos-duration='1000'>
                                     <span className='content-header-wrapper'>
                                         <img src={selv} alt='' className='logo' />
-                                        <h5>A Unified Identity</h5>
+                                        <h5>{t("general.A Unified Identity")}</h5>
                                     </span>
-                                    <p>The Unified Identity Protocol not only enables people and organisations to have the benefits of identity, but devices, opening up a whole new world of business models and use cases.</p>
+                                    <p>{t("views.landing.app.Unified Identity Protocol enables devices")}</p>
                                 </div>
                             </div>
                         </div>
 
                         <Link to='/demo/select'>
                             <Button className='cta' data-aos='fade-up' data-aos-duration='1000'>
-                                Try the demo
+                                {t("actions.Try the demo")}
                 </Button>
                         </Link>
                     </div>
--- web/bank/src/components/landing/Benefits.tsx
+++ web/bank/src/components/landing/Benefits.tsx
@@ -6,49 +6,55 @@ import reusability from '../../assets/landing/benefits/reusability.svg';
 import laws from '../../assets/landing/benefits/laws.svg';
 import responsibility from '../../assets/landing/benefits/responsibility.svg';
 import user from '../../assets/landing/benefits/user.svg';
+import { Translation } from 'react-i18next';
 
 export default () => (
-    <RandomGraphicElement elements={10}>
-        <div className='benefits-section' id='benefits'>
-            <h4 data-aos='fade-up' data-aos-duration='1000'>Benefits</h4>
-            <h2 data-aos='fade-up' data-aos-duration='1000'>For users</h2>
-            <div className='benefits-wrapper'>
-                <div className='benefit' data-aos='fade-up' data-aos-duration='1000'>
-                    <img src={control} alt='Control' />
-                    <h5>Control</h5>
-                    <p>Anything related to your identity and your personal data remains in your control.</p>
-                </div>
-                <div className='benefit' data-aos='fade-up' data-aos-duration='1000'>
-                    <img src={privacy} alt='Privacy' />
-                    <h5>Privacy</h5>
-                    <p>Improved privacy by only revealing the minimum amount of information.</p>
-                </div>
-                <div className='benefit' data-aos='fade-up' data-aos-duration='1000'>
-                    <img src={reusability} alt='Reusability' />
-                    <h5>Reusability</h5>
-                    <p>Share as often as you like. No more typing in the same information repeatedly in online forms.</p>
-                </div>
-            </div>
+    <Translation>
+        {
+            (t, { i18n }) =>
+                <RandomGraphicElement elements={10}>
+                    <div className='benefits-section' id='benefits'>
+                        <h4 data-aos='fade-up' data-aos-duration='1000'>{t("landing.benefits.benefits")}</h4>
+                        <h2 data-aos='fade-up' data-aos-duration='1000'>{t("landing.benefits.forUsers")}</h2>
+                        <div className='benefits-wrapper'>
+                            <div className='benefit' data-aos='fade-up' data-aos-duration='1000'>
+                                <img src={control} alt='Control' />
+                                <h5>{t("landing.benefits.control")}</h5>
+                                <p>{t("landing.benefits.benefitsControl")}</p>
+                            </div>
+                            <div className='benefit' data-aos='fade-up' data-aos-duration='1000'>
+                                <img src={privacy} alt='Privacy' />
+                                <h5>{t("landing.benefits.privacy")}</h5>
+                                <p>{t("landing.benefits.benefitsPrivacy")}</p>
+                            </div>
+                            <div className='benefit' data-aos='fade-up' data-aos-duration='1000'>
+                                <img src={reusability} alt='Reusability' />
+                                <h5>{t("landing.benefits.reusability")}</h5>
+                                <p>{t("landing.benefits.benefitsReusability")}</p>
+                            </div>
+                        </div>
 
-            <h4 data-aos='fade-up' data-aos-duration='1000'>Benefits</h4>
-            <h2 data-aos='fade-up' data-aos-duration='1000'>For business</h2>
-            <div className='benefits-wrapper'>
-                <div className='benefit' data-aos='fade-up' data-aos-duration='1000'>
-                    <img src={laws} alt='Privacy Laws' />
-                    <h5>Privacy Laws</h5>
-                    <p>Complies to the strictest interpretation of the General Data Protection Act (GDPR).</p>
-                </div>
-                <div className='benefit' data-aos='fade-up' data-aos-duration='1000'>
-                    <img src={responsibility} alt=' Data Responsibility' />
-                    <h5>Data Responsibility</h5>
-                    <p>Users can present or consent, removing responsibility on data protection from the company.</p>
-                </div>
-                <div className='benefit' data-aos='fade-up' data-aos-duration='1000'>
-                    <img src={user} alt='User Experience' />
-                    <h5>User Experience</h5>
-                    <p>Retain more customers through an enhanced User Experience. Less manual typing required.</p>
-                </div>
-            </div>
-        </div>
-    </RandomGraphicElement>
+                        <h4 data-aos='fade-up' data-aos-duration='1000'>{t("landing.benefits.benefits")}</h4>
+                        <h2 data-aos='fade-up' data-aos-duration='1000'>{t("landing.benefits.forBusiness")}</h2>
+                        <div className='benefits-wrapper'>
+                            <div className='benefit' data-aos='fade-up' data-aos-duration='1000'>
+                                <img src={laws} alt='Privacy Laws' />
+                                <h5>{t("landing.benefits.privacyLaws")}</h5>
+                                <p>{t("landing.benefits.benefitsPrivacyLaws")}</p>
+                            </div>
+                            <div className='benefit' data-aos='fade-up' data-aos-duration='1000'>
+                                <img src={responsibility} alt=' Data Responsibility' />
+                                <h5>{t("landing.benefits.dataResponsibility")}</h5>
+                                <p>{t("landing.benefits.benefitsDataResponsibility")}</p>
+                            </div>
+                            <div className='benefit' data-aos='fade-up' data-aos-duration='1000'>
+                                <img src={user} alt='User Experience' />
+                                <h5>{t("landing.benefits.userExperience")}</h5>
+                                <p>{t("landing.benefits.benefitsUserExperience")}</p>
+                            </div>
+                        </div>
+                    </div>
+                </RandomGraphicElement>
+        }
+    </Translation>
 );
