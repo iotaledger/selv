1b803e8e809e54ec58b3434878cf44634ba740ec
Eike Haß <eike-hass@web.de>
2024-04-30 10:55:32 +0200

    add web build

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -2,7 +2,7 @@ echo "building the image ..."
 docker compose -f ../docker-compose.yml build --no-cache
 
 echo "shuting down the docker compose ..."
-docker compose -f ../docker-compose.yml -f ../docker-compose.dev.yml down
+docker compose -f ../docker-compose.yml down
 
 echo "starting the docker compose ..."
 docker compose -f ../docker-compose.yml up -d
