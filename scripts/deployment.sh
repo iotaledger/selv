749ee19b6c291a21add327f0576794a1caabd081
junwei0117 <v860117@gmail.com>
2024-04-30 05:11:45 +0000

    chore: add a script for deployment service usage

---
+++ scripts/deployment.sh
@@ -0,0 +1,10 @@
+echo "building the image ..."
+docker compose -f docker-compose.yml -f docker-compose.dev.yml build --no-cache
+
+echo "shuting down the docker compose ..."
+docker compose -f docker-compose.yml -f docker-compose.dev.yml down
+
+echo "starting the docker compose ..."
+docker compose -f docker-compose.yml -f docker-compose.dev.yml up -d
+
+echo "done"
