cb77b1080758aea4c804ba0ca9d73abad87d49c2
Eike Haß <eike-hass@web.de>
2024-08-05 11:41:27 +0200

    enable themes

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import companyHouse from '../assets/companyHouse.svg'
-import snsBank from '../assets/snsBank.svg'
+import { IdcardOutlined, SolutionOutlined } from '@ant-design/icons';
+import { Flex } from 'antd';
+import { useTranslation } from 'react-i18next';
 
 const Header = ({ children, theme }: {
     children?: JSX.Element | null | undefined;
     theme: string | undefined;
 }) => {
+
+    const { t } = useTranslation();
+    
     return (
         <div className='header-wrapper'>
             <div className='logo'>
-                { theme === 'company' && <img src={companyHouse} alt='Company House Logo' /> }
-                { theme === 'bank' && <img src={snsBank} alt='SNS Bank Logo' /> }
-                { theme === 'insurance' && <img src={snsBank} alt='SNS Bank Logo' /> }
+                { theme === 'government' && 
+                    <Flex gap={'small'} align='center'>
+                        <IdcardOutlined/>
+                        <span>{t("steps.government.title")}</span>
+                    </Flex>
+                }
+                { theme === 'company' && 
+                    <Flex gap={'small'} align='center'>
+                        <SolutionOutlined/>
+                        <span>{t("steps.company.title")}</span>
+                    </Flex>
+                }
             </div>
             { children }
         </div>
